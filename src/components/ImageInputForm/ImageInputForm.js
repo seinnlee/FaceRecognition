67ce15280b7bacdf96dfc70a4f1df5e8f656d145
sein <44532313+seinnlee@users.noreply.github.com>
2021-08-09 22:32:28 -0700

    fixed image submission bug

--- src/components/ImageInputForm/ImageInputForm.js
+++ src/components/ImageInputForm/ImageInputForm.js
@@ -10,20 +10,21 @@ const ImageInputForm = ({ onInputChange, onImageSubmit, isSignedIn }) => {
       <div className='center'>
         <div className='form center pa4 br3 shadow-5'>
           <input
-            className='f4 pa2 w-70 center' 
+            className='f4 pa2 w-70 center'
             type='text'
             onChange={onInputChange}
           />
+          {isSignedIn &&
+            <button
+              className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
+              onClick={() => onImageSubmit()}
+            >Detect</button>}
           {!isSignedIn &&
             <button
               className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
               onClick={() => alert("You must sign in to use this feature!")}
             >Detect</button>}
-          {isSignedIn &&
-            <button
-              className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
-              onClick={() => onImageSubmit}
-            >Detect</button>}
+
         </div>
       </div>
     </div>
